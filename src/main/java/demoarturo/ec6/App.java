f4ec8377dab601864027eaacb1913404b610e8ea
arturolatorre <arturola@ymail.com>
2021-09-13 16:42:39 +0200

    first commit

---
+++ src/main/java/demoarturo/ec6/App.java
@@ -0,0 +1,13 @@
+package demoarturo.ec6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
